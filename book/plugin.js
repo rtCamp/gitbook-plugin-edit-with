1936cd36f4d64ec18bc2bba934a478e8ebf58371
Rama Krishna Mundru <ramakrishnamundru471@gmail.com>
2017-06-14 16:22:49 +0530

    Custom icon and option to exclude filepath
    Added option for custom icon and ability to exclude filepath if user only wants to point to the base url

--- book/plugin.js
+++ book/plugin.js
@@ -3,6 +3,8 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         var conf = config['edit-link'];
         var label = conf.label;
         var base = conf.base;
+        var icon = conf.icon;
+        var addpath= conf.addpath ? conf.addpath : true;
         var lang = gitbook.state.innerLanguage;
         if (lang) {
             // label can be a unique string for multi-languages site
@@ -17,12 +19,12 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         }
 
         gitbook.toolbar.createButton({
-            icon: 'fa fa-edit',
+            icon: icon,
             text: label,
             onClick: function() {
                 var filepath = gitbook.state.filepath;
 
-                window.open(base + lang + filepath);
+                window.open(base + lang + addpath ? filepath : '' );
             }
         });
     });
