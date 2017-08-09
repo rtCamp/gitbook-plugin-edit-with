1f0792331cfa0c0c71de2814e64f60459389bc7f
Ewan Edwards <ewan@buddybuild.com>
2017-08-09 13:12:53 -0700

    Add some new configuration:
    * `title` to specify the title attribute for the button.
    * `position` to specify the toolbar position, `left` or `right`.
    * `icon` to specify the button's icon (per per @amakrishnamundru).
    
    Also:
    
    * Defend against undefined GitBook language, per @matsu-chara.
    * Update `README.md`.

--- book/plugin.js
+++ book/plugin.js
@@ -1,14 +1,20 @@
 require(["gitbook", "jQuery"], function(gitbook, $) {
     gitbook.events.bind('start', function (e, config) {
-        var conf = config['edit-link'];
-        var label = conf.label;
-        var base = conf.base;
+        var conf     = config['edit-link'];
+        var base     = conf.base;
+        var icon     = ("icon" in conf)     ? conf.icon     : 'fa fa-edit';
+        var label    = ("label" in conf)    ? conf.label    : "Edit This Page";
+        var title    = ("title" in conf)    ? conf.title    : '';
+        var position = ("position" in conf) ? conf.position : 'left';
+
         var lang = gitbook.state.innerLanguage;
         if (lang) {
             // label can be a unique string for multi-languages site
             if (typeof label === 'object') label = label[lang];
 
             lang = lang + '/';
+        } else {
+          lang = '';
         }
 
         // Add slash at the end if not present
@@ -17,11 +23,12 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         }
 
         gitbook.toolbar.createButton({
-            icon: 'fa fa-edit',
+            icon: icon,
             text: label,
+            label: title,
+            position: position,
             onClick: function() {
                 var filepath = gitbook.state.filepath;
-
                 window.open(base + lang + filepath);
             }
         });
